c0a9eac9f225c5c5e2c30aaf1ebfe4837914f1a2
Andrew A Win <33167031+aaw351@users.noreply.github.com>
2017-10-28 09:46:47 -0400

    Search Library
    Improved aesthetic of search library as well as implementing a search bar.

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const api = require('./api.js');
+
 const port = 8888;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/static/html/index.html");
-});
-
-app.post('/quotes', (req, res) => {
-  console.log('Hellooooo');
+app.get('/*', (req, res) => {
+  res.sendFile(__dirname + req.path);
 });
 
 app.listen(port, function () {
